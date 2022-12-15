2999ed0031d70d78a29612975a84982c99761334
dimchik05 <dima08190@gmail.com>
2022-12-15 19:44:11 +0200

    Create P05.erl

---
+++ P05.erl
@@ -0,0 +1,6 @@
+-module(p05).
+-export([reverse/1]).
+
+reverse(List) -> reverse(List, []).
+reverse([], Reverse_List) -> Reverse_List;
+reverse([H | T], Reverse_List) -> reverse(T, [H | Reverse_List]).
